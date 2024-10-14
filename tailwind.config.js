bfa129265bce0b05e3a594ed9bc5e040c4ea70fa
abhay8696 <kambleabhaygautam@gmail.com>
2024-10-14 14:05:45 +0530

    created color variables in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
     content: ["./index.html", "./src/**/*.{html,js,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                mainBg: "#faffaf",
+                primary: "#0f67b1",
+                skyBlue: "#3fa2f6",
+                lightBlue: "#96c9f4",
+            },
+        },
     },
     plugins: [],
 };
